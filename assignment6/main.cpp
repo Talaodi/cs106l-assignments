ca76340881ee139359fe70c0fa75028ea104a157
Talaodi <2118421487@qq.com>
2026-02-26 15:51:37 +0800

    finished assignment6

--- assignment6/main.cpp
+++ assignment6/main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <type_traits>
 #include <vector>
+#include <optional>
 
 /** STUDENT_TODO: You will need to include a relevant header file here! */
 
@@ -52,10 +53,15 @@ public:
    * @param course_title The title of the course to find.
    * @return You will need to figure this out!
    */
-  FillMeIn find_course(std::string course_title)
+  std::optional<std::vector<Course>::iterator> find_course(std::string course_title)
   {
-    /* STUDENT_TODO: Implement this method! You will need to change the return
-     * type. */
+    auto it = std::ranges::find_if(courses, [&course_title](const auto& course) {
+      return course.title == course_title;
+    });
+    if (it == courses.end()) {
+      return std::nullopt;
+    }
+    return it;
   }
 
 private:
@@ -81,7 +87,11 @@ main(int argc, char* argv[])
     Please pay special attention to the README here
     ********************************************************/
 
-    std::string output = /* STUDENT_TODO */
+    std::string output = course.and_then([](const auto& course) {
+      std::ostringstream out;
+      out << "Found course: " << course->title << "," << course->number_of_units << "," << course->quarter;
+      return std::optional { out.str() };
+    }).value_or("Course not found.");
 
     /********************************************************
      DO NOT MODIFY ANYTHING BELOW THIS LINE PLEASE
