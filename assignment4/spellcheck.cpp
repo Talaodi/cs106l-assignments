bf061dda7b25983ecb4c6c4603f3aafc7409f89c
Talaodi <2118421487@qq.com>
2026-02-26 10:55:20 +0800

    finished assignment4

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -11,13 +11,42 @@ template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
-  /* TODO: Implement this method */
-  return Corpus();
+  auto space_iters = find_all(source.begin(), source.end(), isspace);
+  std::set<Token> tokens;
+  std::transform(
+    space_iters.begin(), space_iters.end() - 1, space_iters.begin() + 1,
+    std::inserter(tokens, tokens.end()),
+    [&source](auto begin, auto end) {
+      return Token(source, begin, end);
+    }
+  );
+  std::erase_if(tokens, [](const auto& token) {
+    return token.content.empty();
+  });
+  return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
-  /* TODO: Implement this method */
-  return std::set<Misspelling>();
+  namespace rv = std::ranges::views;
+  auto view = 
+    source 
+    | rv::filter([&dictionary](const auto& token) { 
+      return !dictionary.contains(token.content); 
+    })
+    | rv::transform([&dictionary](const auto& token) {
+      auto view = 
+        dictionary 
+        | rv::filter([&token](const auto& word) {
+          return levenshtein(token.content, word) == 1;
+        });
+      std::set<std::string> suggestions(view.begin(), view.end());
+      return Misspelling { token, suggestions };
+    })
+    | rv::filter([](const auto& misspelling){
+      return !misspelling.suggestions.empty();
+    });
+
+  return std::set<Misspelling>(view.begin(), view.end());
 };
 
 /* Helper methods */
